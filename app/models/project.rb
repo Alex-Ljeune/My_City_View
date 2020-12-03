6d06d8b4bb41a3761c7096c0b7fd9733c433bd1d
AndyRama <andyramaroson@gmail.com>
2020-12-03 09:35:00 +0100

    limitation 80 caractères pour model projet

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ApplicationRecord
   has_one_attached :photo
   has_many :questions, dependent: :destroy
 
-  validates :description, presence: true
+  validates :description, presence: true, length: { minimum: 10, maximum: 80 }
   validates :title, presence: true
   validates :longitude, presence: true
   validates :latitude, presence: true
